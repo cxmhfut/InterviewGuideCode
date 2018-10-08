d159b664c27e3dfecc3c83d8508d50c6f0a0bc88
cxmhfut <912750125@qq.com>
2018-10-08 10:43:17 +0800

    TestCase for min stack

--- src/chapter1/Chapter1TestCase.java
+++ src/chapter1/Chapter1TestCase.java
@@ -5,7 +5,28 @@ import org.junit.Test;
 public class Chapter1TestCase {
 
     @Test
-    public void testMinStack1(){
+    public void testMinStack2(){
+        MyStack2 stack = new MyStack2();
 
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(1);
+        System.out.println(stack.getMin());
+        stack.pop();
+        System.out.println(stack.getMin());
+    }
+
+    @Test
+    public void testMinStack1() {
+        MyStack1 stack = new MyStack1();
+
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(1);
+        System.out.println(stack.getMin());
+        stack.pop();
+        System.out.println(stack.getMin());
     }
 }
